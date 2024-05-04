b5b452df26c807e5f1eeb453a0aa9051577bf042
GannouniAmine <mohamedamine.gannouni@ensi-uma.tn>
2024-05-04 20:20:21 +0100

    yes

--- Review/review.js
+++ Review/review.js
@@ -15,3 +15,24 @@ function submitReview() {
     reviewElement.textContent = reviewText;
     reviewContainer.appendChild(reviewElement);
 }
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll("header a");
+  
+   
+    links.forEach(function(link) {
+      link.dataset.defaultColor = "#333"; 
+    });
+  
+    
+    links.forEach(function(link) {
+      link.addEventListener("mouseover", function() {
+        this.style.color = "white"; 
+      });
+  
+      link.addEventListener("mouseout", function() {
+        this.style.color = this.dataset.defaultColor; 
+      });
+    });
+  });
