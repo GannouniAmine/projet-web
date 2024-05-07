91d56537289cc32f203a6083d54a1e0585907308
GannouniAmine <mohamedamine.gannouni@ensi-uma.tn>
2024-05-07 13:26:09 +0100

    update

--- signup/signup/sign/sign.js
+++ signup/signup/sign/sign.js
@@ -1,10 +1,10 @@
 function validateForm(){
-    const firstName = document.getElementById('x1').value.trim();
-    const lastName = document.getElementById('x2').value.trim();
-    const username = document.getElementById('x5').value.trim();
-    const email = document.getElementById('x6').value.trim();
-    const password = document.getElementById('x7').value.trim();
-    const confirmPassword = document.getElementById('x8').value.trim();
+    const firstName = document.getElementById('x1').value;
+    const lastName = document.getElementById('x2').value;
+    const username = document.getElementById('x5').value;
+    const email = document.getElementById('x6').value;
+    const password = document.getElementById('x7').value;
+    const confirmPassword = document.getElementById('x8').value;
     let t=false;
     if (firstName === '' || lastName === ''|| username === '' || email === '' || password === '' || confirmPassword === '') {
         alert('Veuillez remplir tous les champs.');
