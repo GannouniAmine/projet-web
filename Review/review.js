6722bf537ca43ebcc90edf9d3c94a93daee4173a
GannouniAmine <mohamedamine.gannouni@ensi-uma.tn>
2024-05-07 13:24:08 +0100

    update

--- Review/review.js
+++ Review/review.js
@@ -11,33 +11,7 @@ function rateProduct(rating) {
 function submitReview() {
     const reviewText = document.getElementById("review").value;
     const reviewContainer = document.getElementById("reviews");
-    const reviewElement = document.createElement("p");
-    reviewElement.textContent = reviewText;
-    reviewContainer.appendChild(reviewElement);
 }
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    const links = document.querySelectorAll("header a");
-  
-   
-    links.forEach(function(link) {
-      link.dataset.defaultColor = "#333"; 
-    });
-  
-    
-    links.forEach(function(link) {
-      link.addEventListener("mouseover", function() {
-        this.style.color = "white"; 
-      });
-  
-      link.addEventListener("mouseout", function() {
-        this.style.color = this.dataset.defaultColor; 
-      });
-    });
-  });
-
-
 function myFunction1() {
     alert("You selected some text!");
 }
