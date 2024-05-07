f92403cce593ebf5f8bbe8741c3615a5da4f2f12
GannouniAmine <mohamedamine.gannouni@ensi-uma.tn>
2024-05-07 11:01:43 +0100

    Update sign.js

--- signup/signup/sign/sign.js
+++ signup/signup/sign/sign.js
@@ -1,20 +1,21 @@
-function validateForm() {
+function validateForm(){
     const firstName = document.getElementById('x1').value.trim();
     const lastName = document.getElementById('x2').value.trim();
     const username = document.getElementById('x5').value.trim();
     const email = document.getElementById('x6').value.trim();
     const password = document.getElementById('x7').value.trim();
     const confirmPassword = document.getElementById('x8').value.trim();
+    let t=false;
     if (firstName === '' || lastName === ''|| username === '' || email === '' || password === '' || confirmPassword === '') {
         alert('Veuillez remplir tous les champs.');
-        return false;
+        return t;
     }
     if (password !== confirmPassword) {
         alert('Les mots de passe ne correspondent pas.');
-        return false; 
+        return t; 
     }
     alert('Formulaire soumis avec succès !');
-    return true;
+    return !t;
 }
 function caractere(){
     chaine="Nous sommes absolument ravis d accueillir notre nouveau client";
